7bb9e37a5566461b8fe557333dff66d6d7226ea1
Manuel Caicedo <manuelcaicedo52@gmail.com>
2026-08-24 16:29:17 -0500

    add prestamos controller

--- src/Controller/PrestamoController.java
+++ src/Controller/PrestamoController.java
@@ -1,4 +1,27 @@
 package Controller;
 
+import model.Prestamo;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrestamoController {
+
+    private List<Prestamo> prestamos;
+
+    public PrestamoController() {
+        prestamos = new ArrayList<>();
+    }
+
+    public void crearPrestamo(Prestamo prestamo) {
+        prestamos.add(prestamo);
+    }
+
+    public List<Prestamo> listarPrestamos() {
+        return prestamos;
+    }
+
+    public void eliminarPrestamo(Prestamo prestamo) {
+        prestamos.remove(prestamo);
+    }
 }
